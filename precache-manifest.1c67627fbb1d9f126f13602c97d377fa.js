c2095f89511b7481fb191de2435f7a017a335ca9
klaratsilva <48393107+klaratsilva@users.noreply.github.com>
2020-06-02 15:46:03 +0100

    Add files via upload

---
+++ precache-manifest.1c67627fbb1d9f126f13602c97d377fa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly_frontend/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "ce8dc2c7336661f433a7",
+    "url": "/vidly_frontend/static/css/main.9bfd5820.chunk.css"
+  },
+  {
+    "revision": "eb1655dd2c006b5640fa",
+    "url": "/vidly_frontend/static/js/runtime~main.eb1655dd.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly_frontend/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7e65ab5503c843c1b19b",
+    "url": "/vidly_frontend/static/js/2.7e65ab55.chunk.js"
+  },
+  {
+    "revision": "ce8dc2c7336661f433a7",
+    "url": "/vidly_frontend/static/js/main.ce8dc2c7.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly_frontend/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly_frontend/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly_frontend/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7e65ab5503c843c1b19b",
+    "url": "/vidly_frontend/static/css/2.4e5a0885.chunk.css"
+  },
+  {
+    "revision": "2ce9f25d0e17eac5a0d63452c2ece6f4",
+    "url": "/vidly_frontend/index.html"
+  }
+];
